541da5d3bcfd37fa86f9f9997f1ad425a98bdd22
Jason Pickens <jasonpickensnz@gmail.com>
2022-07-28 23:18:08 +1200

    Fix detection of story content

--- source/content.ts
+++ source/content.ts
@@ -2,9 +2,9 @@ import $ from 'jquery'
 import browser from 'webextension-polyfill'
 import ClickEvent = JQuery.ClickEvent
 
-const initialisationTimeoutMs = 500
-
 const imageContainer = $('<div>').addClass('sig-image-container')
+
+const postQueryRegex = /[?&]community_post_id=(\d+)(&|$)/
 
 function download(url: string): (event: ClickEvent) => void {
 	return (event: ClickEvent) => {
@@ -38,16 +38,55 @@ function addImageLinks(storyContent: JQuery<HTMLDivElement>) {
 		})
 }
 
-function init() {
-	console.debug('Initializing.')
+function onStoryChange() {
+	console.debug('Story changed.')
+	// This should always be on the page even before the content is loaded.
+	const showPostElement = $<HTMLDivElement>('#show-post')
+	if (showPostElement.length === 0) {
+		throw 'Unable to find #show-post element.'
+	}
 	const storyContent = $<HTMLDivElement>('#story-content')
 	if (storyContent.length > 0) {
 		console.debug('Found story content.')
 		addImageLinks(storyContent)
 	} else {
-		console.debug(`Did not find story content. Waiting ${initialisationTimeoutMs}ms.`)
-		setTimeout(init, initialisationTimeoutMs)
+		console.debug('Story content has not been loaded yet.')
+		const observer = new MutationObserver((mutations, observer) => {
+			const storyContent = $<HTMLDivElement>('#story-content')
+			if (storyContent.length > 0) {
+				console.debug('Found story content.')
+				observer.disconnect()
+				addImageLinks(storyContent)
+			}
+		});
+		observer.observe(showPostElement[0], { childList: true, subtree: true });
 	}
 }
 
-$(init)
+function onPageChange() {
+	console.debug('Page changed.')
+	const pathSegments = window.location.pathname.split('/').filter(segment => segment.length !== 0)
+	if (pathSegments[0] === 'activity') {
+		console.debug('Activity page detected.')
+		if (postQueryRegex.test(window.location.search)) {
+			onStoryChange()
+		}
+	} else {
+		console.debug('Unknown page:', window.location)
+	}
+}
+
+function onLoad() {
+	var location: string = window.location.href
+	onPageChange()
+	const observer = new MutationObserver((mutations, observer) => {
+		if (location !== window.location.href) {
+			location = window.location.href
+			onPageChange()
+		}
+	});
+	console.debug('observing')
+	observer.observe(document, { childList: true, subtree: true });
+}
+
+$(onLoad)
